72ddaf4c572cbf29086a65226458f0a704af8216
Darrington, John Wayne <john.darrington@inl.gov>
2020-10-12 11:43:14 -0600

    removed extra migration

--- migrations/024_nodes_edges_import.sql
+++ migrations/024_nodes_edges_import.sql
@@ -7,3 +7,9 @@ ALTER TABLE imports ADD COLUMN status_message text;
 
 ALTER TABLE nodes ADD COLUMN import_data_id integer REFERENCES data_staging(id) ON DELETE SET NULL;
 ALTER TABLE edges ADD COLUMN import_data_id integer REFERENCES data_staging(id) ON DELETE SET NULL;
+
+DROP TRIGGER IF EXISTS unpack_import on imports;
+DROP FUNCTION IF EXISTS unpack_import_row;
+
+ALTER TABLE imports DROP COLUMN data_json;
+ALTER TABLE imports DROP COLUMN data_csv;
