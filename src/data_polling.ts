3be3acdde4a9ddf1dbdad0bece01ba2731bc7002
jerenmb <jeren.browning@gmail.com>
2020-11-23 08:25:54 -0700

    Update data_polling.ts

--- src/data_polling.ts
+++ src/data_polling.ts
@@ -1,5 +1,5 @@
 // we've created a standalone loop for the Data Polling Loop so as to maximize
-// system resources. The main loop of Deep Lyn will spawn this process
+// system resources. The main loop of Deep Lynx will spawn this process
 
 import {Storage} from "./boot_storage";
 import {StartDataSourcePolling} from "./data_importing/data_source";
