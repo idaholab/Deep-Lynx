63f43e1b852bfeb5ea2172813b749593872e214c
jerenmb <jeren.browning@gmail.com>
2020-11-23 08:26:05 -0700

    Update data_processing.ts

--- src/data_processing.ts
+++ src/data_processing.ts
@@ -1,5 +1,5 @@
 // we've created a standalone loop for the Data Processing Loop so as to maximize
-// system resources. The main loop of Deep Lyn will spawn this process
+// system resources. The main loop of Deep Lynx will spawn this process
 
 import {Storage} from "./boot_storage";
 import {StartDataProcessing} from "./data_processing/processing";
