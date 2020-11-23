8d44520f4410e653811b36891d9f6ce7d49667db
jerenmb <jeren.browning@gmail.com>
2020-11-23 08:25:08 -0700

    Update data_exports.ts

--- src/data_exports.ts
+++ src/data_exports.ts
@@ -1,5 +1,5 @@
 // we've created a standalone loop for the Data Export Loop so as to maximize
-// system resources. The main loop of Deep Lyn will spawn this process
+// system resources. The main loop of Deep Lynx will spawn this process
 
 import {Storage} from "./boot_storage";
 import {RestartExports} from "./data_exporting/exporter";
