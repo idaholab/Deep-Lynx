0f578d18c233cfed7b14e1bb4a208fcf6fba83eb
John Darrington <john.darrington@inl.gov>
2021-08-09 17:31:12 +0000

    fixed graphql query

--- src/http_server/routes/data_warehouse/data/query/query_routes.ts
+++ src/http_server/routes/data_warehouse/data/query/query_routes.ts
@@ -1,8 +1,8 @@
-import { Application, NextFunction, Request, Response } from 'express';
-import { graphql } from 'graphql';
-import { authInContainer } from '../../../../middleware';
+import {Application, NextFunction, Request, Response} from 'express';
+import {graphql} from 'graphql';
+import {authInContainer} from '../../../../middleware';
 import resolversRoot from '../../../../../data_warehouse/data/query/resolvers';
-import { schema } from '../../../../../data_warehouse/data/query/schema';
+import {schema} from '../../../../../data_warehouse/data/query/schema';
 const bodyParser = require('body-parser');
 
 export default class QueryRoutes {
@@ -13,13 +13,24 @@ export default class QueryRoutes {
     // very simple route that passes the raw body directly to the the graphql query
     // for the complex portions of this endpoint visit the data_query folder and functions
     private static query(req: Request, res: Response, next: NextFunction) {
-        graphql(schema, req.body, resolversRoot(req.params.id))
-            .then((response) => {
-                res.status(200).json(response);
-            })
-            .catch((e) => {
-                res.status(500).json(e);
-            })
-            .finally(() => next());
+        if (req.header('content-type') !== 'application/json' && req.header('content-type') !== 'application/graphql') {
+            graphql(schema, req.body, resolversRoot(req.params.id))
+                .then((response) => {
+                    res.status(200).json(response);
+                })
+                .catch((e) => {
+                    res.status(500).json(e);
+                })
+                .finally(() => next());
+        } else {
+            graphql(schema, req.body.query, resolversRoot(req.params.id), req.body.variables)
+                .then((response) => {
+                    res.status(200).json(response);
+                })
+                .catch((e) => {
+                    res.status(500).json(e);
+                })
+                .finally(() => next());
+        }
     }
 }
