94ce6c3df7111941ddff5a97ff8baf87009f5fc1
DARRJW <john.darrington@inl.gov>
2022-01-17 08:43:22 -0700

    corrected timestamp

--- src/data_access_layer/migrations/012_event_system_rework.sql
+++ src/data_access_layer/migrations/012_event_system_rework.sql
@@ -5,5 +5,6 @@
  */
 ALTER TABLE event_action_statuses DROP COLUMN event_id CASCADE;
 ALTER TABLE event_action_statuses ADD COLUMN event jsonb DEFAULT NULL;
+ALTER TABLE data_staging ALTER COLUMN created_at SET DEFAULT clock_timestamp();
 
 DROP TABLE IF EXISTS events;
