caee9a75f1a1659237613659a81dc61e0b88a485
DARRJW <john.darrington@inl.gov>
2022-04-04 16:43:13 -0600

    quick change

--- src/data_processing/process.ts
+++ src/data_processing/process.ts
@@ -186,7 +186,7 @@ export async function ProcessData(staging: DataStaging): Promise<Result<boolean>
         return new Promise((resolve) => resolve(Result.SilentFailure(`error attempting to mark data inserted ${marked.error}`)));
     }
 
-    await stagingRepo.setErrors(staging.id!, []);
+    await stagingRepo.setErrors(staging.id!, [], transaction.value);
     await stagingMapper.completeTransaction(transaction.value);
     return Promise.resolve(Result.Success(true));
 }
