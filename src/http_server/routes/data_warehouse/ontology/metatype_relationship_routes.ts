c7257d1c378ae0ecdbed6e57701c61706ed000b7
John Darrington <john.darrington@inl.gov>
2022-06-08 13:12:28 -0600

    no ontology version returns all

--- src/http_server/routes/data_warehouse/ontology/metatype_relationship_routes.ts
+++ src/http_server/routes/data_warehouse/ontology/metatype_relationship_routes.ts
@@ -87,8 +87,6 @@ export default class MetatypeRelationshipRoutes {
 
         if (typeof req.query.ontologyVersion !== 'undefined' && (req.query.ontologyVersion as string) !== '') {
             repository = repository.and().ontologyVersion('eq', req.query.ontologyVersion);
-        } else {
-            repository = repository.and().ontologyVersion('is null')
         }
 
         if (typeof req.query.deleted !== 'undefined' && String(req.query.deleted as string).toLowerCase() === 'false') {
--- src/http_server/routes/data_warehouse/ontology/metatype_routes.ts
+++ src/http_server/routes/data_warehouse/ontology/metatype_routes.ts
@@ -74,8 +74,6 @@ export default class MetatypeRoutes {
 
         if (typeof req.query.ontologyVersion !== 'undefined' && (req.query.ontologyVersion as string) !== '') {
             repository = repository.and().ontologyVersion('eq', req.query.ontologyVersion);
-        } else {
-            repository = repository.and().ontologyVersion('is null')
         }
 
         if (typeof req.query.modifiedAfter !== 'undefined' && (req.query.modifiedAfter as string) !== '') {
