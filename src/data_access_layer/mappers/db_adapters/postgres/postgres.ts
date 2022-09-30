b518c688fd3ba285ec90a58f3e4bd439e92e184a
John Darrington <john.darrington@inl.gov>
2022-09-30 14:09:00 -0600

    force connection pool

--- src/data_access_layer/mappers/db_adapters/postgres/postgres.ts
+++ src/data_access_layer/mappers/db_adapters/postgres/postgres.ts
@@ -22,6 +22,8 @@ export default class PostgresAdapter {
     // eslint-disable-next-line @typescript-eslint/require-await
     public async init() {
         this.pool = new Pool({
+            min: 10,
+            max: 100,
             connectionString: Config.core_db_connection_string,
             ssl: Config.ssl_enabled
                 ? {
