b33e5c36aa2caae1428bf0349a7cd3d2bc94cd37
John Darrington <john.darrington@inl.gov>
2022-10-02 07:21:05 -0600

    fixed the oauth cache

--- src/services/cache/cache.ts
+++ src/services/cache/cache.ts
@@ -120,9 +120,9 @@ export class RedisCacheImpl implements CacheInterface {
         let set: string;
 
         if (ttl) {
-            set = await this._redis.set(key, JSON.stringify(val), 'EX', ttl);
+            set = await this._redis.set(key, val, 'EX', ttl);
         } else {
-            set = await this._redis.set(key, JSON.stringify(val));
+            set = await this._redis.set(key, val);
         }
 
         if (set !== 'OK') {
