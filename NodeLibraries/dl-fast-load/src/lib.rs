392565e4027e41e5fe8f7e344a00b3e0fa20fd42
John Darrington <john.darrington@inl.gov>
2022-10-09 16:44:05 -0600

    some stuff fixed

--- NodeLibraries/dl-fast-load/src/lib.rs
+++ NodeLibraries/dl-fast-load/src/lib.rs
@@ -1,53 +1,119 @@
 extern crate core;
 
 use neon::prelude::*;
+use neon::result::Throw;
 use neon::types::buffer::TypedArray;
+use once_cell::sync::OnceCell;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::PgConnection;
+use std::borrow::Borrow;
+use std::future::Future;
 use std::io::{BufReader, Read};
-use std::sync::{Arc, mpsc, Mutex};
-use std::sync::mpsc::{Receiver, Sender};
+use std::ops::Deref;
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::time::Instant;
-use neon::result::Throw;
+use tokio::runtime::Runtime;
+use tokio::sync::futures;
+use tokio::sync::mpsc::Sender;
+
+// Return a global tokio runtime or create one if it doesn't exist.
+// Throws a JavaScript exception if the `Runtime` fails to create.
+fn runtime<'a, C: Context<'a>>(cx: &mut C) -> NeonResult<&'static Runtime> {
+    static RUNTIME: OnceCell<Runtime> = OnceCell::new();
+
+    RUNTIME.get_or_try_init(|| Runtime::new().or_else(|err| cx.throw_error(err.to_string())))
+}
 
 pub struct Manager {
-    channel: Sender<ManagerMessage>,
+    channel: mpsc::Sender<ManagerMessage>,
 }
 
 impl<'a> Finalize for Manager {}
 
 impl Manager {
     fn new(mut cx: FunctionContext) -> JsResult<JsBox<Manager>> {
+        //let connection_string = cx.argument::<JsString>(0)?.value(&mut cx);
+        let connection_string = String::from("postgresql://darrjw@localhost:5433/deep_lynx");
+        let rt = runtime(&mut cx)?;
         let (tx, rx) = mpsc::channel::<ManagerMessage>();
 
-        thread::spawn(move || {
+        rt.spawn(async move {
+            let pool = PgPoolOptions::new()
+                .max_connections(5)
+                .connect(connection_string.borrow())
+                .await;
+
+            println!("Connections established");
+            let (mut tx1, mut rx1) = tokio::sync::mpsc::channel::<CopyMessage>(2048);
+
+            rt.spawn(async move {
+                let start = Instant::now();
+                let mut copier = pool
+                    .unwrap()
+                    .copy_in_raw("COPY test_table(bldg_number) FROM STDIN WITH (FORMAT csv)")
+                    .await;
+                let mut copier = copier.unwrap();
+
+                loop {
+                    let message = rx1.recv().await;
+                    match message {
+                        None => {}
+                        Some(m) => match m {
+                            CopyMessage::Copy(message) => {
+                                copier.send(message.as_slice()).await;
+                            }
+                            CopyMessage::Close => {
+                                println!("finished");
+                                break;
+                            }
+                        },
+                    }
+                }
+
+                let bob = copier.finish().await;
+                println!("{:?}", bob);
+                let duration = start.elapsed();
+                println!("Time elapsed in writing 1 million rows is: {:?}", duration);
+            });
+
             let stream = NodeStream::new(rx);
             let stream = BufReader::with_capacity(4096, stream);
             let mut i = 0;
 
             let mut rdr = csv::ReaderBuilder::new().flexible(true).from_reader(stream);
-            let start = Instant::now();
-            for result in rdr.records() {
+
+            while let Some(result) = rdr.records().next() {
                 // The iterator yields Result<StringRecord, Error>, so we check the
                 // error here..
-                let record = result;
+                let record = result.unwrap();
+                tx1.send(CopyMessage::Copy(
+                    [
+                        record.as_byte_record().as_slice().to_vec(),
+                        String::from("\n").into_bytes(),
+                    ]
+                    .concat(),
+                ))
+                .await;
+
                 // println!("{:?}", record);
-                i += 1
+
+                i += 1;
             }
 
-            let duration = start.elapsed();
-            println!("Time elapsed in reading {:?} rows is: {:?}", i, duration);
+            tx1.send(CopyMessage::Close).await;
         });
 
-
-        return Ok(cx.boxed(Manager {
-            channel: tx
-        }));
+        return Ok(cx.boxed(Manager { channel: tx }));
     }
 
     fn read(mut cx: FunctionContext) -> JsResult<JsNull> {
         let manager = cx.argument::<JsBox<Manager>>(0)?;
         let null = cx.null();
-        let chunk = cx.argument::<JsTypedArray<u8>>(1)?.as_slice(&mut cx).to_vec();
+        let chunk = cx
+            .argument::<JsTypedArray<u8>>(1)?
+            .as_slice(&mut cx)
+            .to_vec();
 
         manager.channel.send(ManagerMessage::Write(chunk)).unwrap();
 
@@ -64,6 +130,11 @@ impl Manager {
     }
 }
 
+enum CopyMessage {
+    Copy(Vec<u8>),
+    Close,
+}
+
 // Messages sent on the database channel
 enum ManagerMessage {
     // Promise to resolve and callback to be executed
@@ -75,13 +146,13 @@ enum ManagerMessage {
 }
 
 pub struct NodeStream {
-    channel: Receiver<ManagerMessage>,
+    channel: mpsc::Receiver<ManagerMessage>,
     buffer: Vec<u8>,
     is_closed: bool,
 }
 
 impl NodeStream {
-    fn new(rx: Receiver<ManagerMessage>) -> Self {
+    fn new(rx: mpsc::Receiver<ManagerMessage>) -> Self {
         return NodeStream {
             channel: rx,
             buffer: vec![],
@@ -99,7 +170,6 @@ impl Read for NodeStream {
         if self.buffer.len() >= buf.len() {
             let send = self.buffer.clone();
             self.buffer = vec![];
-
 
             if (send.len() > buf.len()) {
                 let (dest, overflow) = send.split_at(buf.len());
@@ -134,7 +204,6 @@ impl Read for NodeStream {
 
         let send = self.buffer.clone();
         self.buffer = vec![];
-
 
         if (send.len() > buf.len()) {
             let (dest, overflow) = send.split_at(buf.len());
