3107b3c39025cf617b3b78f0bde50e99b708335a
John Darrington <john.darrington@inl.gov>
2022-12-12 09:01:22 -0700

    fixed duplicates

--- src/data_access_layer/migrations/052_edges_unique.sql
+++ src/data_access_layer/migrations/052_edges_unique.sql
@@ -1,1 +1,8 @@
+DELETE FROM edges e USING edges b
+       WHERE e.id > b.id
+         AND e.origin_id = b.origin_id
+         AND e.destination_id = b.destination_id
+         AND e.relationship_pair_id = b.relationship_pair_id
+         AND e.created_at = b.created_at;
+
 CREATE UNIQUE INDEX IF NOT EXISTS edges_uniq_idx ON edges (container_id,relationship_pair_id,data_source_id,created_at, origin_id, destination_id);
