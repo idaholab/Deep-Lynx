2ab90dc3ca3ea12d0c9cded398c157f1e05d8235
Nathan Woodruff <nathan.woodruff@inl.gov>
2023-02-21 15:44:12 -0700

    Remove unused code - hardcoded uri

--- WebGLViewer/app/store/thunks/fetchWebGL.ts
+++ WebGLViewer/app/store/thunks/fetchWebGL.ts
@@ -2,19 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const fetchWebGL = createAsyncThunk('webgl/fetch', async () => {
-  const response = await axios.get('http://localhost:8090/containers/2/files/5/download');
-
-  // DEV ONLY!!!
-  await pause(1000);
-
-  return response.data;
+  
 });
-
-// DEV ONLY!!!
-const pause = (duration: any) => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, duration);
-  });
-};
 
 export { fetchWebGL };
