c6616e5121597350f59d1948efa26d7f14f07856
Nathan Woodruff <nathan.woodruff@inl.gov>
2023-02-21 11:14:53 -0700

    Add jwt handling in request query

--- src/http_server/authentication/jwt.ts
+++ src/http_server/authentication/jwt.ts
@@ -9,7 +9,7 @@ export function SetJWTAuthMethod(app: express.Application) {
     passport.use(
         new JwtStrategy(
             {
-                jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+                jwtFromRequest: ExtractJwt.fromExtractors([ExtractJwt.fromAuthHeaderAsBearerToken(), ExtractJwt.fromUrlQueryParameter("auth_token")]),
                 secretOrKey: Config.encryption_key_secret,
                 algorithms: ['RS256'],
             },
