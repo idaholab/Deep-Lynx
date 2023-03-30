7fc729a14f9fb5bec5f255308c31f1de9978a270
Adam Pluth <adam.pluth@inl.gov>
2023-03-30 15:42:16 -0600

    added nearby nodes table

--- WebGLViewer/src/components/drawercontents/nodeinfo/NodeInfoNearbyNodes.tsx
+++ WebGLViewer/src/components/drawercontents/nodeinfo/NodeInfoNearbyNodes.tsx
@@ -1,8 +1,13 @@
 // React
 import * as React from 'react';
 
+// Hooks
+import { useState, useEffect } from 'react';
+import { useAppSelector, useAppDispatch } from '../../../../app/hooks/reduxTypescriptHooks';
+
 // Import Packages
 import Plot from 'react-plotly.js';
+import axios from 'axios';
 
 // MUI Components
 import {
@@ -22,93 +27,73 @@ type Props = {
   data: any
 };
 
-const NodeInfoPropsMeta: React.FC<Props> = ({
+const NodeInfoNearbyNodes: React.FC<Props> = ({
   data
 }) => {
+  const nodeId = data.id
+  const [tableRowData, setTableRowData] = useState([]);
 
-  const PropertiesTableHeaders = [
+  const tableHeaders = [
+    {title: 'Id', alignment: 'left'},
     {title: 'Name', alignment: 'left'},
-    {title: 'Value', alignment: 'left'},
+    {title: 'Relationship', alignment: 'left'},
+    {title: 'Actions', alignment: 'center'},
   ];
 
-  // const tableData = data;
-  const PropertiesTableRowData = [
-    {
-      id: 1,
-      name: 'id',
-      value: 'st7',
-    },
-    {
-      id: 2,
-      name: 'name',
-      value: 'shack',
-    },
-    {
-      id: 3,
-      name: 'fruits',
-      value: 'pear',
-    },
-    {
-      id: 4,
-      name: 'vegetables',
-      value: 'carrot',
-    },
-    {
-      id: 5,
-      name: 'grains',
-      value: 'wheat',
-    },
-  ];
+  type webGLFileSetId = Array<{ [key: string]: any; }>;
+  const webGLFileSetId = useAppSelector((state: any) => state.appState.selectedWebGLFileSetId);
 
-  const PropertiesTableRowActions = [
-    {type: 'edit',},
-    {type: 'delete',}
-  ];
+  useEffect(() => {
+    async function getNearbyNodes() {
+      const files = JSON.parse(localStorage.getItem('node')!);
+      const token = "bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGl0eV9wcm92aWRlciI6InNhbWxfYWRmcyIsImRpc3BsYXlfbmFtZSI6Ik5hdGhhbiBMLiBXb29kcnVmZiIsImVtYWlsIjoiTmF0aGFuLldvb2RydWZmQGlubC5nb3YiLCJhZG1pbiI6ZmFsc2UsImFjdGl2ZSI6dHJ1ZSwicmVzZXRfcmVxdWlyZWQiOmZhbHNlLCJlbWFpbF92YWxpZCI6ZmFsc2UsInR5cGUiOiJ1c2VyIiwicGVybWlzc2lvbnMiOltdLCJyb2xlcyI6W10sInVzZXJfaWQiOiIxOSIsImtleSI6Ik16SXlNVFJoTm1JdE9EYzNNaTAwWWpjMExXRmxaRFF0TkdOaE16VmlOR0l6TVdaaiIsInNlY3JldCI6IiQyYSQxMCRJREg5WXpGM2RmeXVpNDA2ZVFVSWhPU3Y1djQ2czNMaTlGTERJVlc4a2NpeERnZThNclpiMiIsIm5vdGUiOiJBZGFtIiwiaWQiOiIxOSIsImlkZW50aXR5X3Byb3ZpZGVyX2lkIjoiTmF0aGFuLldvb2RydWZmQGlubC5nb3YiLCJjcmVhdGVkX2F0IjoiMjAyMi0wOC0xOFQwNjowMDowMC4wMDBaIiwibW9kaWZpZWRfYXQiOiIyMDIyLTA4LTE4VDA2OjAwOjAwLjAwMFoiLCJjcmVhdGVkX2J5Ijoic2FtbC1hZGZzIGxvZ2luIiwibW9kaWZpZWRfYnkiOiJzYW1sLWFkZnMgbG9naW4iLCJyZXNldF90b2tlbl9pc3N1ZWQiOm51bGwsImlhdCI6MTY3ODkxMzA5OCwiZXhwIjoxNzEwNDcwNjk4fQ.6ex5ftZOQlOEkCev-G54qPE2waN3HZeDsMpK4ssPa_fEamUhd7-qt56OcM87VUSMNEDRYVPw74WF9PhyBUf2n4EslunCUZLYYpW1RdrQViDcbi6zHPlfMe0KrRwTsu4YobAVEMvEYkpA_wW0u0O4YfemEZdrqORYxMONHfmqCC_tA4FODi5hrv9ln3xH3DpmUaVXlvRzzFvPH5bE-jKV_gdmeunkFyfVuE1wuUx7I0jF6ZpUj1u09bikdvTo-FmchkRRGyYKKsbu5H3DZsr7JoT_tAnS_Z5O9MBeHu8uqciAc2esElkq3t_cpxi5yLriIx5mSEI6b7Fb6UEClVP5XA";
 
-  const MetadataTableHeaders = [
-    {title: 'Name', alignment: 'left'},
-    {title: 'Value', alignment: 'left'},
-  ];
+      // await axios.get ( `localhost:8090/containers/1/graphs/nodes/${nodeId}/graph?depth=1`,
+      await axios.get ( "http://localhost:8090/containers/1/graphs/nodes/90/graph?depth=1",
 
-  // const tableData = data;
-  const MetadataTableRowData = [
-    {
-      id: 1,
-      name: 'id',
-      value: 'st7',
-    },
-    {
-      id: 2,
-      name: 'name',
-      value: 'shack',
-    },
-    {
-      id: 3,
-      name: 'fruits',
-      value: 'pear',
-    },
-    {
-      id: 4,
-      name: 'vegetables',
-      value: 'carrot',
-    },
-    {
-      id: 5,
-      name: 'grains',
-      value: 'wheat',
-    },
-  ];
+        {
+          headers: {
+            Authorization: token
+          }
+        }).then (
+          (response: any) => {
+            setTableRowData(() => {
+              let nearbyNodes: any = [];
+              let receivedData = response.data.value;
+              console.log(receivedData)
+              receivedData.map((obj: any) => {
+                return (
+                  nearbyNodes.push({
+                    id: obj.destination_id,
+                    name: obj.destination_properties.name,
+                    relationship: obj.relationship_name,
+                    // direction: obj.edge_direction
+                  })
+                )
+              })
+              return (
+                nearbyNodes
+              )
+            })
+          }
+        )
+    }
 
-  const MetadataTableRowActions = [
-    {type: 'view',},
-    {type: 'delete',}
+    getNearbyNodes();
+  }, []);
+
+  const tableRowActions = [
+    {type: 'delete'}
   ];
 
   return (
     <>
-      <p>Nearby Nodes go here</p>
+      <InfoHeader>
+        Nearby Nodes
+      </InfoHeader>
+      <DataTableBasic tableHeaders={tableHeaders} tableRowData={tableRowData} tableRowActions={tableRowActions} />
     </>
   );
 }
 
-export default NodeInfoPropsMeta;
+export default NodeInfoNearbyNodes;
