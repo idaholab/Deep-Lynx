83bdd15ebcfc7e8cafd803aa02c16ef891bd7a1a
John Darrington <john.darrington@inl.gov>
2023-05-23 08:33:49 -0600

    test update

--- NodeLibraries/deeplynx-timeseries/__test__/index.spec.mjs
+++ NodeLibraries/deeplynx-timeseries/__test__/index.spec.mjs
@@ -74,7 +74,7 @@ test('bucket retrieval', async (t) => {
     }
 })
 
-/*test('ingestion async test', async (t) => {
+test('ingestion async test', async (t) => {
     let repo = new BucketRepository();
     await repo.init({
         dbConnectionString: "postgresql://postgres:deeplynxcore@localhost/deep_lynx",
@@ -121,7 +121,7 @@ test('bucket retrieval', async (t) => {
     } catch (e) {
         return Promise.reject(e)
     }
-})*/
+})
 
 test('legacy ingestion async test', async (t) => {
     let repo = new BucketRepository();
@@ -131,7 +131,7 @@ test('legacy ingestion async test', async (t) => {
     })
     // should create a new bucket as names are not unique
     try {
-        repo.beginLegacyCsvIngestion('46', [
+        repo.beginLegacyCsvIngestion('CHANGE ME', [
             {
                 is_primary_timestamp: true,
                 column_name: "time",
