5d7df6a380b72511a762c49d040b6f2ddd028d62
John Darrington <john.darrington@inl.gov>
2023-06-18 12:17:11 -0600

    add redis graph loader and simple endpoint for testing

--- NodeLibraries/deeplynx-timeseries/__test__/index.spec.mjs
+++ NodeLibraries/deeplynx-timeseries/__test__/index.spec.mjs
@@ -131,7 +131,7 @@ test('legacy ingestion async test', async (t) => {
     })
     // should create a new bucket as names are not unique
     try {
-        repo.beginLegacyCsvIngestion('46', [
+        repo.beginLegacyCsvIngestion('CHANGE ME', [
             {
                 is_primary_timestamp: true,
                 column_name: "time",
@@ -175,7 +175,7 @@ test('legacy ingestion async test', async (t) => {
                     .catch((e) => reject(e))
             });
 
-            let stream = fs.createReadStream('./test_files/1milliontestlegacy.csv');
+            let stream = fs.createReadStream('./test_files/sparse_legacy.csv');
             stream.pipe(pass);
         })
     } catch (e) {
