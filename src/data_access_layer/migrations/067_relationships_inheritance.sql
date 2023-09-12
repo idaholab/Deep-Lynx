bc75a225121b1170d341d1f037901aee2fa0c1fb
Jeren Browning <jeren.browning@inl.gov>
2023-09-12 10:54:18 -0600

    Fix migration files

--- src/data_access_layer/migrations/067_relationships_inheritance.sql
+++ src/data_access_layer/migrations/067_relationships_inheritance.sql
@@ -82,14 +82,21 @@ $$ LANGUAGE sql;
  */
 DROP FUNCTION IF EXISTS get_metatype_keys;
 
-CREATE OR REPLACE FUNCTION get_metatype_keys(arg_metatype_id bigint, arg_container_id bigint) RETURNS setof record
+CREATE OR REPLACE FUNCTION public.get_metatype_keys(arg_metatype_id bigint, arg_container_id bigint)
+    RETURNS SETOF metatype_full_keys
+    LANGUAGE sql
 AS $$
 WITH RECURSIVE parents AS (
     SELECT * FROM metatypes_view
     WHERE id = arg_metatype_id
     UNION
     SELECT v.* from metatypes_view v INNER JOIN parents p ON p.parent_id = v.id WHERE v.container_id = arg_container_id
-) SELECT metatype_keys.*, p.name AS metatype_name FROM parents p INNER JOIN metatype_keys ON metatype_keys.metatype_id = p.id;
+) SELECT mk.id, mk.metatype_id, p.name AS metatype_name, mk.name,
+         mk.description, mk.required, mk.property_name, mk.data_type,
+         mk.options, mk.default_value, mk.validation, mk.created_at,
+         mk.modified_at, mk.created_by, mk.modified_by, mk.ontology_version,
+         mk.container_id, mk.deleted_at
+FROM parents p INNER JOIN metatype_keys mk ON mk.metatype_id = p.id;
 $$ LANGUAGE sql;
 
 /*
