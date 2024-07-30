fffa13af8cc5291936e4ce8aeacce55ebd49dbcd
Michael Harding <Michael.Harding@inl.gov>
2024-07-30 09:26:04 -0600

    update lib.rs to include correct ts2 mods (also autoformatting via )

--- server/NodeLibraries/deeplynx/src/lib.rs
+++ server/NodeLibraries/deeplynx/src/lib.rs
@@ -2,6 +2,8 @@
 #[macro_use]
 extern crate napi_derive;
 
+mod config;
+pub mod errors;
 /// The structure of the DeepLynx package is fairly simple, but we will note it is continually evolving.
 /// It originally started out as completely separate packages. As we did more with Rust however, we
 /// realized we needed to pull these packages into one package and start unifying the interface as well
@@ -11,7 +13,6 @@ extern crate napi_derive;
 /// so that we can share the underlying connections and memory spaces.
 pub mod redis_graph;
 pub mod shape_hasher;
-pub mod errors;
-mod config;
 mod snapshot;
-
+mod timeseries;
+mod timeseries2;
