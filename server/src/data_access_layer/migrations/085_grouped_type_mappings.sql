6a22662a006c5c57d6e93244328a5beba5871a2d
dylan-kuncheria <Dylan.Kuncheria@inl.gov>
2024-08-15 10:25:56 -0600

    Updating hash_groupings table and creating grouped view

--- server/src/data_access_layer/migrations/085_grouped_type_mappings.sql
+++ server/src/data_access_layer/migrations/085_grouped_type_mappings.sql
@@ -1,3 +1,22 @@
+--  Drop the tsvector column
+ALTER TABLE hash_groupings DROP COLUMN shape_hash_tsvector;
+
+--  Change the column definitions
+ALTER TABLE hash_groupings DROP CONSTRAINT hash_groupings_pkey;
+ALTER TABLE hash_groupings ALTER COLUMN hash_grouping_id SET DATA TYPE bigint;
+ALTER TABLE hash_groupings ADD PRIMARY KEY (hash_grouping_id);
+ALTER TABLE hash_groupings ADD FOREIGN KEY (hash_grouping_id) REFERENCES type_mappings(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+--  Ensure the unique constraint is still valid
+ALTER TABLE hash_groupings ADD CONSTRAINT unique_type_mapping_shape_hash UNIQUE(type_mapping_id, shape_hash);
+
+--  Drop the index on tsvector if exists
+DROP INDEX IF EXISTS idx_new_shape_hashes_tsvector;
+
+--  Remove the primary key constraint
+ALTER TABLE public.hash_groupings
+DROP CONSTRAINT IF EXISTS hash_groupings_pkey;
+
 DROP VIEW IF EXISTS grouped_type_mappings;
 CREATE VIEW grouped_type_mappings AS
 SELECT 
@@ -16,26 +35,3 @@ FROM
 WHERE 
     id NOT IN (SELECT hash_grouping_id FROM public.hash_groupings);
 
---  Drop the tsvector column
-ALTER TABLE hash_groupings DROP COLUMN shape_hash_tsvector;
-
---  Change the column definitions
-ALTER TABLE hash_groupings DROP CONSTRAINT hash_groupings_pkey;
-ALTER TABLE hash_groupings ALTER COLUMN hash_grouping_id SET DATA TYPE bigint;
-ALTER TABLE hash_groupings ADD PRIMARY KEY (hash_grouping_id);
-ALTER TABLE hash_groupings ADD FOREIGN KEY (hash_grouping_id) REFERENCES type_mappings(id) ON UPDATE CASCADE ON DELETE CASCADE;
-
---  Ensure the unique constraint is still valid
-ALTER TABLE hash_groupings ADD CONSTRAINT unique_type_mapping_shape_hash UNIQUE(type_mapping_id, shape_hash);
-
---  Drop the index on tsvector if exists
-DROP INDEX IF EXISTS idx_new_shape_hashes_tsvector;
-
-
---  Remove the primary key constraint
-ALTER TABLE public.hash_groupings
-DROP CONSTRAINT IF EXISTS hash_groupings_pkey;
-
---  Ensure uniqueness on (type_mapping_id, shape_hash)
-ALTER TABLE public.hash_groupings
-ADD CONSTRAINT unique_type_mapping_shape_hash UNIQUE (type_mapping_id, shape_hash);
