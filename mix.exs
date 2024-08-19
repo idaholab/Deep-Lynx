57b8f7b261291e3ddcd7052a829931f036cba817
dnoberon <johnw.darrington@gmail.com>
2024-08-19 15:35:12 -0400

    ran user auth generator

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule Datum.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:argon2_elixir, "~> 3.0"},
       {:phoenix, "~> 1.7.14"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.10"},
