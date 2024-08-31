4d4eae95545fc482805501491c7cecee2139f29d
dnoberon <johnw.darrington@gmail.com>
2024-08-31 17:31:15 -0400

    commit before doing user groups

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -14,6 +14,41 @@ module.exports = {
       },
     },
   },
+  daisyui: {
+    themes: [
+      "retro",
+      {
+        light: {
+          primary: "#2b49ff",
+          secondary: "#00b5b4",
+          accent: "#ff301c",
+          neutral: "#0f1023",
+          "base-100": "#fff",
+          "base-200": "#f6f7fb",
+          "base-300": "#fafbfc",
+          info: "#98d7f0",
+          success: "#9cdcc5",
+          warning: "#e0ca96",
+          error: "#f8bbb0",
+        },
+      },
+      {
+        dark: {
+          primary: "#2b49ff",
+          secondary: "#00b5b4",
+          accent: "#ff301c",
+          neutral: "#4e5168",
+          "base-100": "#393b51",
+          "base-200": "#2b2d43",
+          "base-300": "#0f1023",
+          info: "#98d7f0",
+          success: "#9cdcc5",
+          warning: "#e0ca96",
+          error: "#f8bbb0",
+        },
+      },
+    ],
+  },
   plugins: [
     require("@tailwindcss/forms"),
     require("daisyui"),
