ea3fa21bc617d1f5ba96fb8b0dfca92969b5a929
Jack Cavaluzzi <Jack.Cavaluzzi@inl.gov>
2024-09-17 13:27:38 -0600

    Update setPayload in page.tsx

--- ui/next-ui/src/app/(main)/containers/[containerID]/model-viewer/[modelID]/page.tsx
+++ ui/next-ui/src/app/(main)/containers/[containerID]/model-viewer/[modelID]/page.tsx
@@ -53,9 +53,9 @@ export default function Home({ params }: { params: { modelID: string } }) {
     ConfigType: "Remote",
     FileName: "MRV-1000.glb",
     GraphType: "cad",
-    GraphRootDlId: "5123079",
+    GraphRootDlId: "2",
     AssetMetatypeName: "MeshGameObject",
-    DefaultInteractions: ["Highlight", "Draggable"],
+    DefaultInteractions: ["CadNodeDataToReact", "SelectAndFadeOthers"],
     MetatypeMappings: {
       CADMetadata: "Metadata",
       Quality: "JsonObjectsToReact",
@@ -67,6 +67,25 @@ export default function Home({ params }: { params: { modelID: string } }) {
     ContainerId: container.id,
     FileId: file.id,
   };
+
+  const domePayload = {
+    ConfigType: "Remote",
+    FileName: "dome_loading_dock.glb",
+    GraphType: "cad",
+    GraphRootDlId: "2",
+    AssetMetatypeName: "MeshGameObject",
+    DefaultInteractions: ["CadNodeDataToReact", "SelectAndFadeOthers"],
+    MetatypeMappings: {
+      CADMetadata: "Metadata",
+      Requirement: "JsonObjectsToReact",
+    },
+    BaseUrl: "https://deeplynx.azuredev.inl.gov",
+    Token:
+      "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGl0eV9wcm92aWRlciI6InNhbWxfYWRmcyIsImRpc3BsYXlfbmFtZSI6IkphY2sgTS4gQ2F2YWx1enppIiwiZW1haWwiOiJKYWNrLkNhdmFsdXp6aUBpbmwuZ292IiwiYWRtaW4iOmZhbHNlLCJhY3RpdmUiOnRydWUsInJlc2V0X3JlcXVpcmVkIjpmYWxzZSwiZW1haWxfdmFsaWQiOmZhbHNlLCJ0eXBlIjoidXNlciIsInBlcm1pc3Npb25zIjpbXSwicm9sZXMiOltdLCJ1c2VyX2lkIjoiNSIsImtleSI6Ill6VmpObVl5TTJVdE1URmxZUzAwTVdKbUxXSXhaREl0TkRrMk1XVmpaVGd6TW1VdyIsInNlY3JldCI6IiQyYSQxMCRjdW85MGtOMHJPTndldmFwSmM2alYuTVdiSGhYYi9xNWl6ekZYY2lQbzNOdzA0Zmh5TzNLaSIsIm5vdGUiOiJBSEEiLCJpZCI6IjUiLCJpZGVudGl0eV9wcm92aWRlcl9pZCI6IkphY2suQ2F2YWx1enppQGlubC5nb3YiLCJjcmVhdGVkX2F0IjoiMjAyMi0wMy0xNFQwNjowMDowMC4wMDBaIiwibW9kaWZpZWRfYXQiOiIyMDIyLTAzLTE0VDA2OjAwOjAwLjAwMFoiLCJjcmVhdGVkX2J5Ijoic2FtbC1hZGZzIGxvZ2luIiwibW9kaWZpZWRfYnkiOiJzYW1sLWFkZnMgbG9naW4iLCJyZXNldF90b2tlbl9pc3N1ZWQiOm51bGwsImlhdCI6MTcwMTg3OTcwOSwiZXhwIjoxNzcwOTk5NzA5fQ.k98-_uK77bzVL3jp2vzWWbANAqJDPDV7jXDRqlmhY0-rsZPhjBSbFR09robMPIlHuqbfcELtt4cHrLKSitvrymVPrvH78R7Gd3FhJ6lIP_Yqk7N2VNOy7Q42wlbOCR6-nICFS90AdZ7AHRP5AW8mqlmcWgPJvdFXo2Qz6Y1cRvLb0_6Ei6zmRLM1zyKu_ZyPhhBD42aZWkbZDcutKQOIR7K7KfmaTB4eTzKz3JjxAJ3gMXPmZHC3R3KAUMwMggNAgSK7mg6IAPOyE4E158KJr1lOVoBACgoOIbcXdc0sZiFdPvGPjO9FYhZc4XtjPWYmlHLo2NcnT_OXeu4E5Q7_Ag",
+    ContainerId: container.id,
+    FileId: file.id,
+  };
+
   useEffect(() => {
     let MetatypeMappings: MetatypeMappingsT = mappings.reduce(
       (accumulator, mapping) => {
@@ -82,9 +101,9 @@ export default function Home({ params }: { params: { modelID: string } }) {
       ConfigType: "Remote",
       FileName: file.file_name,
       GraphType: "cad",
-      GraphRootDlId: "5119985", // Must come from Pixyz
+      GraphRootDlId: "2", // Must come from Pixyz; now using Pixyz' NodeId instead of DeepLynxID; the root Pixyz' NodeId should always be "2" (at least according to all the CAD models I've tested); however, we can make this more robust, and the best way might be for React to receive info back from Airflow https://github.inl.gov/Digital-Engineering/Pythagoras/issues/17
       AssetMetatypeName: "MeshGameObject",
-      DefaultInteractions: ["Highlight", "Draggable"],
+      DefaultInteractions: ["CadNodeDataToReact", "SelectAndFadeOthers"],
       MetatypeMappings: MetatypeMappings,
       BaseUrl: "https://deeplynx.azuredev.inl.gov",
       Token:
