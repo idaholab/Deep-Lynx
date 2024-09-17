a6bc59d6096dbd084798c9e06fd56239fad0bf67
Jeren Browning <Jeren.Browning@inl.gov>
2024-09-17 06:50:45 -0600

    Feat/helm airflow (#753)
    * Adds airflow to the helm chart as well as a solution for load balancer port forwarding through the nginx controller
    
    * Expose airflow and postgres
    
    * Add chart to dockerignore
    
    * conditionally printed success statement
    
    * fixed issue with inconsistent return code variable
    
    ---------
    
    Co-authored-by: Ed Ramos <edward.ramos@inl.gov>

--- chart/helm-refresh.sh
+++ chart/helm-refresh.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 
+error_occurred=false
+
 printf "Refreshing helm release...\n\n"
 
 # uninstall current release
-helm uninstall dl1 --namespace deeplynx
+helm uninstall dl1
 
 printf "\n"
 
 # delete persistnce volume claims
-kubectl delete pvc --all --namespace deeplynx
+kubectl delete pvc --all
+if [ $? -ne 0 ]; then error_occurred=true; fi
 
 printf "\n"
 
 # delete any old deeplynx helm packages
 find . -name '*.tgz' -delete -maxdepth 1
+if [ $? -ne 0 ]; then error_occurred=true; fi
 
 # create a new helm package
 helm package .
+if [ $? -ne 0 ]; then error_occurred=true; fi
 
 printf "\n"
 
 # install the new package
-helm install dl1 deeplynx-0.1.0.tgz --namespace deeplynx --create-namespace
+helm install dl1 deeplynx-0.1.0.tgz
+if [ $? -ne 0 ]; then error_occurred=true; fi
 
-printf "\nSuccessful refresh!\n\n"
+if $error_occurred; then
+  printf "\nRefreshed with potential issues.\n\n"
+else
+  printf "\nSuccessful refresh!\n\n"
+fi
 
 kubectl get pods -A -o wide
 
