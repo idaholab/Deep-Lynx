f8f14fda79f893ace578f7394891de9364d66615
Jake Swinford <Jake.Swinford@inl.gov>
2024-10-30 15:21:35 -0600

    formatter

--- ui/Next/src/app/(auth)/login/page.tsx
+++ ui/Next/src/app/(auth)/login/page.tsx
@@ -1,13 +1,15 @@
 // React
-import * as React from 'react';
-import {Login} from './login';
+import * as React from "react";
+import { Login } from "./login";
 
 // Components
 
 export default async function Page() {
-  return (
-    <>
-      <Login />
-    </>
-  );
+  return (
+    <>
+            
+      <Login />
+          
+    </>
+  );
 }
--- ui/Next/tailwind.config.ts
+++ ui/Next/tailwind.config.ts
@@ -22,6 +22,6 @@ export default {
   plugins: [require("@tailwindcss/forms")],
   darkMode: "selector",
   corePlugins: {
-    preflight: true, // Apply tailwind styles only after MUI has applied its native styles and theme to our UX components
+    preflight: false, // Apply tailwind styles only after MUI has applied its native styles and theme to our UX components
   },
 } satisfies Config;
