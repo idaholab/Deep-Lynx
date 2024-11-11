26e0bf4131ad15ce345eee0de0f8ea7f3a22c2b9
Jake Swinford <Jake.Swinford@inl.gov>
2024-11-11 09:24:19 -0700

    spacing / updating failureRedirect

--- server/src/http_server/routes/access_management/oauth_routes.ts
+++ server/src/http_server/routes/access_management/oauth_routes.ts
@@ -58,8 +58,8 @@ export default class OAuthRoutes {
         app.get(
             '/oauth/oidc',
             passport.authenticate('openidconnect', {
-                successReturnToOrRedirect: '/#',
-                failureRedirect: '/oauth',
+                successReturnToOrRedirect: '/',
+                failureRedirect: '/unauthorized',
             }),
         );
         app.get('/check-oidc', (req, res) => {
