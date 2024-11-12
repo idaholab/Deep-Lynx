9051e71e3ed2a1f4fefd7bc2ca7e5dd0aaaf5427
Jake Swinford <Jake.Swinford@inl.gov>
2024-11-12 10:04:00 -0700

    add profile icon

--- ui/Next/src/app/(main)/containers/page.tsx
+++ ui/Next/src/app/(main)/containers/page.tsx
@@ -33,13 +33,7 @@ import {
 import {
   Bars3Icon,
   BellIcon,
-  CalendarIcon,
-  ChartPieIcon,
   Cog6ToothIcon,
-  DocumentDuplicateIcon,
-  FolderIcon,
-  HomeIcon,
-  UsersIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
 import {
