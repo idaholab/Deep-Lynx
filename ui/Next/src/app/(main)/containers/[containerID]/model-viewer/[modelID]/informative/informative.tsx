7fcd01bcb37d6137699a5a93c6dea6c9c2ee87b9
Nathan Woodruff <Nathan.Woodruff@inl.gov>
2024-11-21 12:30:48 -0600

    Update informative.tsx

--- ui/Next/src/app/(main)/containers/[containerID]/model-viewer/[modelID]/informative/informative.tsx
+++ ui/Next/src/app/(main)/containers/[containerID]/model-viewer/[modelID]/informative/informative.tsx
@@ -3,7 +3,14 @@ import { Typography } from "@mui/material";
 export default function Informative() {
   return (
     <>
-      <Typography variant={"h4"}>Live View</Typography>
+      <Typography variant="subtitle1">Informative Twin</Typography>
+      <Typography variant="caption">
+        An informative digital twin integrates data streams to produce a
+        stateful representation of the physical system. The informative twin
+        yields real-time insights and visualizations.
+      </Typography>
+      <br />
+      <br />
     </>
   );
 }
