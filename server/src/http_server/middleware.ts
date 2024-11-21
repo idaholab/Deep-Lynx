e12c4a32878f9a2bb24ecf4f945ba49df7a1caa0
Jake Swinford <Jake.Swinford@inl.gov>
2024-11-21 14:46:31 -0700

    redirect

--- server/src/http_server/middleware.ts
+++ server/src/http_server/middleware.ts
@@ -117,7 +117,7 @@ export function authenticateRoute(): any {
                     if (req.isAuthenticated()) {
                         next();
                     } else {
-                        resp.status(401).send('unauthorized');
+                        resp.redirect('/oauth');
                     }
                 }
             };
