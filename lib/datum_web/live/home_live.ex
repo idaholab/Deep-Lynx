be401242715506b6990776769774ad05ad7a2e8c
John W. Darrington <John.Darrington@inl.gov>
2024-12-06 10:12:28 -0500

    quick fix

--- lib/datum_web/live/home_live.ex
+++ lib/datum_web/live/home_live.ex
@@ -88,7 +88,7 @@ defmodule DatumWeb.HomeLive do
             ) %>
           <% end %>
           <div :if={closed_tabs(group_index, @selected_tabs, @tabs)}>
-            <div class="divide-y divide-accent overflow-hidden rounded-lg bg-base-300 shadow sm:grid sm:grid-cols-2 sm:gap-px sm:divide-y-0">
+            <div class="divide-y divide-accent overflow-hidden rounded-lg bg-base-100 shadow sm:grid sm:grid-cols-2 sm:gap-px sm:divide-y-0">
               <div
                 phx-click="open_tab"
                 phx-value-group={group_index}
@@ -175,44 +175,6 @@ defmodule DatumWeb.HomeLive do
                   </h3>
                   <p class="mt-2 text-sm text-primary-content">
                     <%= gettext("Chat with an AI Assistant Description") %>
-                  </p>
-                </div>
-                <span
-                  class="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400"
-                  aria-hidden="true"
-                >
-                  <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                    <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
-                  </svg>
-                </span>
-              </div>
-              <div class="hover:bg-base-300 group relative bg-base-200 p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
-                <div>
-                  <span class="inline-flex rounded-lg bg-base-300 p-3 text-white ring-4 ring-base-400">
-                    <svg
-                      class="h-6 w-6"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
-                      />
-                    </svg>
-                  </span>
-                </div>
-                <div class="mt-8">
-                  <h3 class="text-base font-semibold leading-6 text-primary-content">
-                    <a href="#" class="focus:outline-none">
-                      <span class="absolute inset-0" aria-hidden="true"></span> Payroll
-                    </a>
-                  </h3>
-                  <p class="mt-2 text-sm text-primary-content">
-                    Doloribus dolores nostrum quia qui natus officia quod et dolorem. Sit repellendus qui ut at blanditiis et quo et molestiae.
                   </p>
                 </div>
                 <span
