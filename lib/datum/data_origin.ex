8ac65d65b4353d8fc8c929809e642bad3fbf872e
John W. Darrington <John.Darrington@inl.gov>
2025-01-17 12:17:48 -0500

    added formatting

--- lib/datum/data_origin.ex
+++ lib/datum/data_origin.ex
@@ -112,8 +112,11 @@ defmodule Datum.DataOrigin do
                )
            }) do
       # connecting to data origin record to establish database
-      Datum.DataOrigin.OriginRepo.with_dynamic_repo(updated_origin, fn -> {:ok, updated_origin} end,
-      mode: :readwrite)
+      Datum.DataOrigin.OriginRepo.with_dynamic_repo(
+        updated_origin,
+        fn -> {:ok, updated_origin} end,
+        mode: :readwrite
+      )
     else
       err -> err
     end
